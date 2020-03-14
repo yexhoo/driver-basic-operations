b51b8827ab3b9d68e9669a02a63a8ddf17d93f07
yexhoo (Josue Nolasco) <josue.nolasco@gmail.com>
2020-03-14 16:54:23 -0600

    Ban a driver rules were added

---
+++ src/executors/ban-executor.js
@@ -0,0 +1,33 @@
+const cache = require("../cache")
+const codes = require("../codes")
+const util = require("../util")
+
+exports.get = () => {
+    return (operation) => {
+
+        if (!cache.DRIVERS.has(operation.driver)) {
+            operation.violations = []
+            operation.violations.push(codes.DRIVER_DOES_NOT_EXIST)
+        }
+
+        return ban(operation)
+    }
+}
+
+const ban = (operation) => {
+
+    const driver = util.copy(cache.DRIVERS.get(operation.driver))
+    if (driver.onRide) {
+        driver.violations.push(codes.DRIVER_ON_RIDE)
+        return driver
+    }
+
+    if (driver.status === codes.DRIVER_BANNED) {
+        driver.violations.push(codes.DRIVER_ALREADY_BANNED)
+        return driver
+    }
+
+    driver.status = codes.DRIVER_BANNED
+    cache.DRIVERS.set(operation.driver, util.copy(driver))
+    return driver
+}
